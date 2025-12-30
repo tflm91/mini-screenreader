b8f0ce398f267e2c667fc1dedfbc7b2f1c7f4f38
tflm91 <79469963+tflm91@users.noreply.github.com>
2025-12-30 13:24:04 +0000

    implement describe function

---
+++ semantics/describe.py
@@ -0,0 +1,17 @@
+from roles import ROLE_LABELS
+
+def describe(element): 
+    parts = []
+
+    if element.name:
+        parts.append(element.name)
+
+    role = element.role
+    parts.append(ROLE_LABELS.role)
+
+    if element.checked is True: 
+        parts.apped("aktiviert")
+    if element.checked is False: 
+        parts.append("nicht aktiviert")
+
+    return ", ".join(parts)
